c2a7dec521657ddb1575e5c1fe2c0e555370cc0f
Artix6034 <erlenrymhan08@gmail.com>
2025-12-12 00:13:08 +0500

    1x2 added constructor and printname

--- src/Property.java
+++ src/Property.java
@@ -3,5 +3,15 @@ public class Property {
     private String address;
     private int price;
     private double area;
+    public Property(String name, String address, int price, double area) {
+        this.name = name;
+        this.address = address;
+        this.price = price;
+        this.area = area;
+    }
+    public String printname() {
+        System.out.println("Name of the property: ");
+        return name;
+    }
 
 }
